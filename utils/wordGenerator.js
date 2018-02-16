02494f6a5f5c709ad5e493d7ac07aa4fc878191b
kulik maxim <kulikmaxim92@gmail.com>
2018-02-16 23:51:52 +0300

    some configuration of wordnik api

--- utils/wordGenerator.js
+++ utils/wordGenerator.js
@@ -2,17 +2,12 @@ const fetch = require('node-fetch');
 
 function getWord() {
     const key = 'a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
-    // const url = `http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionary` +
-    //     `Def=false&includePartOfSpeech=noun&minCorpusCount=0&maxCorpusCount=-1&` +
-    //     `minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=${apiKey}`;
     const corpus = 100000; 
     const def = true;
     const type = 'noun';
     const exludeType = 'noun-plural';
     const url = `http://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=${def}` +
         `&includePartOfSpeech=${type}&excludePartOfSpeech=${exludeType}&minCorpusCount=${corpus}&api_key=${key}`;
-
-
 
     return fetch(url)
         .then((response) => response.json())
